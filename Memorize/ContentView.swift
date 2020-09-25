5df7fec52751fee10fd946ee6c6968fe7c7d06ba
Richard <richardmenning@googlemail.com>
2020-09-25 23:40:18 +0200

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Richard Menning on 06.09.20.
+//  Copyright © 2020 Richard Menning. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .foregroundColor(Color.orange).padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻").font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
